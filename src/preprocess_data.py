d179530945084c475d052dae650de01cfe02024e
Ilaria <manco.ilaria@gmail.com>
2020-04-13 21:45:21 +0100

    Add script to preprocess data

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -3,17 +3,12 @@
 
 import torchaudio
 import config_file
-import os
-import argparse
 import pickle
-import json
+import os
 from pathlib import Path
 
 
-text_file = "download_links.txt"
-
-
-def compute_melspectrograms(audio_file, melspec_file):
+def compute_melspectrograms(audio_file, melspec_file, config):
     audio, sr = torchaudio.load(audio_file)
     # TODO currently no resampling
     mel_spectrogram = torchaudio.transforms.MelSpectrogram(sample_rate=sr, 
@@ -28,7 +23,7 @@ def compute_melspectrograms(audio_file, melspec_file):
     return length
 
 
-def preprocess(files):
+def preprocess(files, config):
     for file_index, file_to_process in enumerate(files):
         try:
             [file_id, audio_file, melspec_file] = files[file_index]
@@ -36,7 +31,7 @@ def preprocess(files):
                 path = Path(melspec_file[:melspec_file.rfind('/') + 1])
                 path.mkdir(parents=True, exist_ok=True)
                 # TODO move block below out of if statement after first rerun
-            length = compute_melspectrograms(audio_file, melspec_file)
+            length = compute_melspectrograms(audio_file, melspec_file, config)
             # index.tsv writing
             fw = open(config_file.DATA_PATH + config['melspectrogram_path'] + ".tsv", "a")
             fw.write("%s\t%s\t%s\n" % (file_id, melspec_file[len(config_file.DATA_PATH):], audio_file[len(config_file.DATA_PATH):]))
@@ -51,31 +46,3 @@ def preprocess(files):
             print('Error computing audio representation: ', audio_file)
             print(str(e))
 
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('configurationID', help='ID of the configuration dictionary')
-    args = parser.parse_args()
-    config = config_file.config_preprocess[args.configurationID]
-
-    # 1. Make new directory for the mel spectrograms
-    config["melspectrogram_path"] = config['identifier'] + "/%s_mels/" % (config['identifier'])
-    # set audio representations folder
-    if not os.path.exists(config_file.DATA_PATH + config['melspectrogram_path']):
-        os.makedirs(config_file.DATA_PATH + config['melspectrogram_path'])
-    
-    # 2. Find audio files to preprocess
-    files_to_preprocess = []
-    f = open(config_file.DATA_PATH + config["index_file"])
-    for line in f.readlines():
-        file_id, audio = line.strip().split("\t")
-        melspectrogram = audio[:audio.rfind(".")] + ".pk"  # .npy or .pk
-        # (id, path to audio file, path to mel spectrogram)
-        files_to_preprocess.append((file_id, config["audio_path"] + audio,
-                                    config_file.DATA_PATH + config["melspectrogram_path"] + melspectrogram))
-    
-    # 3. Compute mel spectrograms
-    preprocess(files_to_preprocess)
-    # 4. Save the parameters in a json
-    json.dump(config, open(config_file.DATA_PATH + config['melspectrogram_path'] + "config.json", "w"))
